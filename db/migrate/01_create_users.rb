1254c7f6ab13c92eda6b336b4100d4c0c8c49d40
Leslie Osorio <lesosorio17@gmail.com>
2019-11-13 17:37:26 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :username
       t.string :password
-      t.decimal :balance
+      t.integer :balance
 
     end
   end
